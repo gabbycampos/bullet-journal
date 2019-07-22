fc4f63597780f46afc16afa69aeeb98cf0f9ff0e
Gabby Campos <gabbycampos@Gabbys-MacBook-Pro.local>
2019-07-21 20:27:48 -0500

    Updates

---
+++ precache-manifest.12001e17082fcd0a40180b72fbb2e632.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f731d8421a9996eee3f6cb6d41c2566",
+    "url": "/bullet-journal/index.html"
+  },
+  {
+    "revision": "c17ea24bf9054de51115",
+    "url": "/bullet-journal/static/css/main.e5584f8d.chunk.css"
+  },
+  {
+    "revision": "047c9eb0439788119091",
+    "url": "/bullet-journal/static/js/2.efb512d5.chunk.js"
+  },
+  {
+    "revision": "c17ea24bf9054de51115",
+    "url": "/bullet-journal/static/js/main.8556c967.chunk.js"
+  },
+  {
+    "revision": "77d7b31da352b40b42af",
+    "url": "/bullet-journal/static/js/runtime~main.872f98d9.js"
+  }
+]);
